97b64c34b90cdec800d64649118bba6eac889379
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-01-10 02:03:00 +0900

    Additional task function (text)

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// 追加ボタンをクリックした際の関数
+
+const onClickAdd = () => {
+	// テキストボックスの値を取得し、初期化する
+	const inputText = document.getElementById('add-text').value;
+	document.getElementById('add-text').value = '';
+
+	// 未完了TODOのdivを生成
+	const div = document.createElement('div');
+	div.className = 'list-row';
+
+	// 未完了TODOのliを生成
+	const li = document.createElement('li');
+	li.innerText = inputText;
+
+	// divタグの子要素に各要素を設定
+	div.appendChild(li);
+
+	// 未完了リストに追加
+	document.getElementById('incomplete-list').appendChild(div);
+};
+
+document
+	.getElementById('add-button')
+	.addEventListener('click', () => onClickAdd());
